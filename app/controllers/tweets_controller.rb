ff460e81a1f6fa8ed9683b41cec0e120dc23eba6
岩本隆之 <iwamototakayuki@iwamototakayukinoMacBook-Pro.local>
2017-12-30 18:56:06 +0900

    tweetの削除、編集のアクションを定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,28 +3,19 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
-  end
-
-  def destroy
-    tweet = Tweet.find(params[:id])
-    tweet.destroy if tweet.user_id == current_user.id
-  end
-
-  def edit
-    @tweet = Tweet.find(params[:id])
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
   def tweet_params
-    params.permit(:image, :text)
+    params.permit(:name, :image, :text)
   end
 
   def move_to_index
