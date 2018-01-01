defcf603a297fc7c54bdba6a22f8e0bdbf8573f3
岩本隆之 <iwamototakayuki@iwamototakayukinoMacBook-Pro.local>
2018-01-01 13:48:00 +0900

    showメソッドの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,6 +29,10 @@ class TweetsController < ApplicationController
     end
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
