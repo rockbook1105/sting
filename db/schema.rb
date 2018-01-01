040642d6175ffb6337e3820c2a3300e96a73d1f4
岩本隆之 <iwamototakayuki@iwamototakayukinoMacBook-Pro.local>
2018-01-01 13:52:58 +0900

    commentテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230055612) do
+ActiveRecord::Schema.define(version: 20180101043628) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "tweet_id",   limit: 4
+    t.text     "text",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", force: :cascade do |t|
-    t.string   "name",       limit: 255
     t.text     "text",       limit: 65535
     t.text     "image",      limit: 65535
     t.datetime "created_at"
