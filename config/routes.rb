a6e75697761b776b648098bf85d0cf9435df7f4a
岩本隆之 <iwamototakayuki@iwamototakayukinoMacBook-Pro.local>
2018-01-01 23:32:44 +0900

    ルーティングをネストさせて、tweetのidをcommentに反映させた

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
   resources :tweets do
-    resources :commmets, only: [:create]
+    resources :comments, only: [:create]
   end
   resources :users, only: [:show]
 end
